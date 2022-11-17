1fc0e55bdd09d3499653282f2b42913422133035
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-11-17 21:04:00 +0530

    some unwanted files json data , assests local images and demo code removed

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,29 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import {PostviewPage,LandingPage} from './Layouts/index';
 import { BrowserRouter,Route, Routes} from 'react-router-dom';
 function App() {
-const [name,setName]=useState("")
-function displayName(){
-  // console.log("clikc")
-  alert(name)
-}
   return (
     <div className="App">
-
-    <input type="text" name="firstname" value={name} onChange={(e)=>setName(e.target.value)}/>   
-   <button onClick={()=>displayName()}>submit</button>
-     {/* <BrowserRouter>
+     <BrowserRouter>
         <Routes>
           <Route path="/" element={  <LandingPage />}>
           </Route>
           <Route path="/Postview" element={  <PostviewPage />}>
           </Route>
         </Routes>
-      </BrowserRouter> */}
-      {/* <p>hfusdf</p> */}
-      
+      </BrowserRouter>       
     </div>
   );
 }
